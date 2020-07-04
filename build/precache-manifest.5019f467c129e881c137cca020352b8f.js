5e1f930ceeee14906e20ab49a7f7aa1904b6ac15
nandkumargangai1@outlook.com <nandkumargangai1@outlook.com>
2020-07-04 12:07:47 +0530

    Added build.

---
+++ build/precache-manifest.5019f467c129e881c137cca020352b8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f222ac7d5a010c7a49bd46c926ab0d73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89268667abae24f8aa00",
+    "url": "/static/css/main.8aefb735.chunk.css"
+  },
+  {
+    "revision": "cec8196fb42d92b7a1dc",
+    "url": "/static/js/2.5a6afebb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5a6afebb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89268667abae24f8aa00",
+    "url": "/static/js/main.eb073e22.chunk.js"
+  },
+  {
+    "revision": "459f34d5f09d1038095c",
+    "url": "/static/js/runtime-main.b6d60773.js"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/static/media/logo.3795f66d.png"
+  }
+]);
