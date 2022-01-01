855ec9787dda38196bff182671ba4b35e26cf8f0
Nandkumar Gangai <nandkumargangai1@outlook.com>
2022-01-01 20:26:51 +0530

    Added gh-pages

---
+++ build/precache-manifest.1d1f8822f693fed3831420299cea4af9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd792469901429b9b848d9a19793b0d5",
+    "url": "/breaking-bad-cast/index.html"
+  },
+  {
+    "revision": "07748dcb2c8f274c74da",
+    "url": "/breaking-bad-cast/static/css/main.8aefb735.chunk.css"
+  },
+  {
+    "revision": "cec8196fb42d92b7a1dc",
+    "url": "/breaking-bad-cast/static/js/2.5a6afebb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breaking-bad-cast/static/js/2.5a6afebb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07748dcb2c8f274c74da",
+    "url": "/breaking-bad-cast/static/js/main.711c79d8.chunk.js"
+  },
+  {
+    "revision": "d04a94b0f37a0168113c",
+    "url": "/breaking-bad-cast/static/js/runtime-main.fee2975c.js"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breaking-bad-cast/static/media/logo.3795f66d.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5019f467c129e881c137cca020352b8f.js"
+  "/breaking-bad-cast/precache-manifest.1d1f8822f693fed3831420299cea4af9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/breaking-bad-cast/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
